e459fa0d5ef530267c7d5cf9652e10a681ee3e68
Simran Sidhu <narmis@live.unc.edu>
2018-11-13 14:57:29 -0500

    One test (testRegionConstructor)

---
+++ src/a6test/narmis99/A6Tests.java
@@ -0,0 +1,34 @@
+package a6test.narmis99;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import a6.Region;
+import a6.RegionImpl;
+
+public class A6Tests {
+
+	@Test
+	public void testRegionConstructor() {
+		Region reg;
+		Region other;
+		Region another;
+		
+		try {
+			reg = new RegionImpl(5, 2, 3, 3);
+			other = new RegionImpl(1, 5, 3, 3);
+		}
+		catch (IllegalArgumentException e) {
+			
+		}
+		
+		try {
+			another = new RegionImpl(1, 3, 5, -7);
+		}
+		catch (IllegalArgumentException e) {
+			
+		}
+	}
+
+}
