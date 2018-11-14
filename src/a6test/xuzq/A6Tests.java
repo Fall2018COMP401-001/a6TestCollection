7fd1a737a7aeaafa29a60c0f09d722b33ad06a48
ziqianxu <ziqianxu@ziqians-mbp.wireless-1x.unc.edu>
2018-11-13 19:25:02 -0500

    tests

---
+++ src/a6test/xuzq/A6Tests.java
@@ -0,0 +1,14 @@
+package a6test.xuzq;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class A6Tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
