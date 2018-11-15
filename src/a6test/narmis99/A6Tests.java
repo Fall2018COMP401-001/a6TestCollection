35c60f0caedfdeef4d6215c80801eea2e89a00ba
Simran Sidhu <narmis@live.unc.edu>
2018-11-14 20:12:47 -0500

    One test (testRegionConstructor)

--- src/a6test/narmis99/A6Tests.java
+++ src/a6test/narmis99/A6Tests.java
@@ -1,26 +1,23 @@
-package a6test.narmis99;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import a6.NoIntersectionException;
+import a6.ObservablePicture;
+import a6.ObservablePictureImpl;
+import a6.Picture;
+import a6.PictureImpl;
+import a6.ROIObserver;
 import a6.Region;
 import a6.RegionImpl;
 
 public class A6Tests {
-
+	
 	@Test
 	public void testRegionConstructor() {
 		Region reg;
 		Region other;
 		Region another;
-		
-		try {
-			another = new RegionImpl(1, 3, 5, -7);
-		}
-		catch (IllegalArgumentException e) {
-			
-		}
 		
 		try {
 			reg = new RegionImpl(5, 2, 3, 3);
@@ -30,6 +27,25 @@ public class A6Tests {
 			
 		}
 		
+		try {
+			another = new RegionImpl(1, 3, 5, -7);
+		}
+		catch (IllegalArgumentException e) {
+			
+		}
 	}
+	
+	/*@Test
+	public void testObservableNull() {
+		ROIObserver obs = null;
+		Region reg = new RegionImpl(1, 3, 5, 7);		
+		
+		try {
+			registerROIObserver(obs, reg);
+		}
+		catch (IllegalArgumentException e) {
+			
+		}
+	}*/
 
 }
