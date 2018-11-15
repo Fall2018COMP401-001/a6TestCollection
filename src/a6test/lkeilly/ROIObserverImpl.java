88866e10182ad7c6535e0ac2144ce111d217f71a
lkeilly <lkeilly@lkeilly-pc.wireless-1x.unc.edu>
2018-11-15 13:18:12 -0500

    extra-credit sub

---
+++ src/a6test/lkeilly/ROIObserverImpl.java
@@ -0,0 +1,27 @@
+package a6test.lkeilly;
+
+import a6.*;
+import a6.ObservablePicture;
+import a6.ROIObserver;
+import a6.Region;
+
+public class ROIObserverImpl implements ROIObserver {
+
+	private int counter;
+
+	public ROIObserverImpl() {
+				counter = 0;
+			}
+
+	public int getCount() {
+		return counter;
+	}
+
+	public void notify(ObservablePicture picture, Region changed_region) {
+		counter++;
+	}
+
+	public void clearCounter() {
+		counter = 0;
+	}
+}
