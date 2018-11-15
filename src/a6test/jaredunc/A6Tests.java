8851b1ee98a7c699b1f4d4d10128cbebff092772
jaredunc <jaredunc@jaredunc-pc.wireless-1x.unc.edu>
2018-11-15 18:12:55 -0500

    jaredunc

--- src/a6test/jaredunc/A6Tests.java
+++ src/a6test/jaredunc/A6Tests.java
@@ -1,5 +1,36 @@
 package a6test.jaredunc;
 
+import a6;
+
+
+
 public class A6Tests {
 	
+	public void testIntersect() throws NoIntersectionException {
+		Region a = new RegionImpl(0, 0, 4, 4);
+		Region b = new RegionImpl(0, 0, 7, 7);
+
+		Region c = b.intersect(a);
+
+		assertEquals(0, a.getLeft());
+		assertEquals(0, a.getTop());
+		assertEquals(4, a.getRight());
+		assertEquals(4, a.getBottom());
+
+
+	}
+	
+	public void testUnion() {
+		
+		Region a = new RegionImpl(0, 0, 4, 4);
+		Region b = new RegionImpl(0, 0, 7, 7);
+		
+		Region c = b.union(a);
+		
+		assertEquals(0, a.getLeft());
+		assertEquals(0, a.getTop());
+		assertEquals(7, a.getRight());
+		assertEquals(7, a.getBottom());
+		
+	}
 }
