23967ce1fe93754de94d802d8f718a7621cf13df
limeng <limeng@limengdembp.wireless-1x.unc.edu>
2018-11-16 10:16:48 -0500

    First Upload

---
+++ src/a6test/mengl/ROIObserverImpl.java
@@ -0,0 +1,13 @@
+package a6test.mengl;
+
+import a6.ObservablePicture;
+import a6.ROIObserver;
+import a6.Region;
+
+public class ROIObserverImpl implements ROIObserver {
+
+	@Override
+	public void notify(ObservablePicture picture, Region changed_region) {
+		System.out.print("Region Change");
+	}
+}
