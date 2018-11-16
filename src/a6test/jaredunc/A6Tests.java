7fa9886cd9e9e7edb611416f2a864c3a414a0e9a
jaredunc <jaredunc@jaredunc-pc.wireless-1x.unc.edu>
2018-11-16 17:23:13 -0500

    jaredunc

--- src/a6test/jaredunc/A6Tests.java
+++ src/a6test/jaredunc/A6Tests.java
@@ -6,30 +6,30 @@ package a6test.jaredunc;
 public class A6Tests {
 	
 	public void testIntersect() throws NoIntersectionException {
-		Region a = new RegionImpl(0, 0, 4, 4);
-		Region b = new RegionImpl(0, 0, 7, 7);
+		Region a = new RegionImpl(1, 1, 5, 6);
+		Region b = new RegionImpl(0, 0, 10, 10);
 
 		Region c = b.intersect(a);
 
-		assertEquals(0, a.getLeft());
-		assertEquals(0, a.getTop());
-		assertEquals(4, a.getRight());
-		assertEquals(4, a.getBottom());
+		assertEquals(1, a.getLeft());
+		assertEquals(1, a.getTop());
+		assertEquals(5, a.getRight());
+		assertEquals(6, a.getBottom());
 
 
 	}
 	
 	public void testUnion() {
 		
-		Region a = new RegionImpl(0, 0, 4, 4);
-		Region b = new RegionImpl(0, 0, 7, 7);
+		Region a = new RegionImpl(1, 1, 5, 6);
+		Region b = new RegionImpl(0, 0, 2, 2);
 		
 		Region c = b.union(a);
 		
 		assertEquals(0, a.getLeft());
 		assertEquals(0, a.getTop());
-		assertEquals(7, a.getRight());
-		assertEquals(7, a.getBottom());
+		assertEquals(5, a.getRight());
+		assertEquals(6, a.getBottom());
 		
 	}
 }
