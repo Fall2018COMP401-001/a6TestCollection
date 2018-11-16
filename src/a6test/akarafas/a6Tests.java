6d1836c273698dc7bdfaee5419bbd9a5fedfee17
akarafas <akarafas@akarafas-pc.wireless-1x.unc.edu>
2018-11-16 14:25:42 -0500

    NegativeUnionTest, RegionImplGetters, and ROIObserverImplGetter

--- src/a6test/akarafas/a6Tests.java
+++ src/a6test/akarafas/a6Tests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import a6.Region;
+import a6.*;
 import a6.RegionImpl;
 
 public class a6Tests {
@@ -16,6 +16,23 @@ public class a6Tests {
 		System.out.println(negativeB.union(negativeA).getLeft());
 		Region aUnionB = new RegionImpl(-20,-10,-10,-5);
 		assertEquals(aUnionB.getLeft(), negativeB.union(negativeA).getLeft());
+	}
+	
+	@Test
+	public void RegionImplGetters() {
+		Region checkThisRegion = new RegionImpl(1546,2343,3454,4564);
+		assertEquals(1546, checkThisRegion.getLeft());
+		assertEquals(2343, checkThisRegion.getTop());
+		assertEquals(3454, checkThisRegion.getRight());
+		assertEquals(4564, checkThisRegion.getBottom());
+	}
+	
+	@Test
+	public void ROIObserverImplGetter() {
+		Region r = new RegionImpl(1,2,3,4);
+		ROIObserver observingR = new ROIObserverImpl(r);
+		assertEquals(r, observingR.getRegion());
+		
 	}
 	
 }
