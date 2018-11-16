b4417e2d80b192ae8bbfafdf218e86adaa7ed0cd
bentontc <btheissen@yahoo.com>
2018-11-15 19:05:30 -0500

    bentontc test

---
+++ src/a6test/bentontc/a6tests.java
@@ -0,0 +1,14 @@
+package a6test.bentontc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class a6tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
